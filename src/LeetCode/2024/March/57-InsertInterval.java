51db1c4f60c1c0622fea5413241da7ec4ce845f3
Veena Kumari <30977545+VKSGit2017@users.noreply.github.com>
2024-03-17 15:51:07 -0700

    Create 57-InsertInterval.java
    Leetcode 17th March question

---
+++ src/LeetCode/2024/March/57-InsertInterval.java
@@ -0,0 +1,34 @@
+/*
+57 Insert Interval 
+You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+https://leetcode.com/problems/insert-interval/description/
+*/
+
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int len = intervals.length;
+        int start =0;
+        List<int[]> result = new ArrayList<>();
+
+        while(start < len && intervals[start][1]< newInterval[0]){
+            result.add(intervals[start]);
+            start++;
+        }
+
+        while(start < len && intervals[start][0]<= newInterval[1]){
+            newInterval[0] = Math.min(intervals[start][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[start][1], newInterval[1]);
+            start++;
+        }
+        result.add(newInterval);
+
+         while(start < len){
+            result.add(intervals[start]);
+            start++;
+        }
+        return result.toArray(new int[result.size()][]);
+    }
+}
+
